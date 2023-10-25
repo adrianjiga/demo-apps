8f4af9cceae6a50dad69dd9f8653c34cb76f1859
Adrian Jiga <jiga.ion.adrian@gmail.com>
2023-10-25 15:47:31 +0300

    [to-do-list] add code for index, script and style files

---
+++ JavaScript/simple-apps/to-do-list/script.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+	const enterButton = document.getElementById("enter");
+	const input = document.getElementById("userInput");
+	const ul = document.querySelector("ul");
+
+	function getInputLength() {
+		return input.value.length;
+	}
+
+	function createListElement() {
+		const li = document.createElement("li");
+		li.appendChild(document.createTextNode(input.value));
+		ul.appendChild(li);
+		input.value = "";
+
+		li.addEventListener("click", function () {
+			li.classList.toggle("done");
+		});
+
+		const deleteButton = document.createElement("button");
+		deleteButton.appendChild(document.createTextNode("X"));
+		li.appendChild(deleteButton);
+		deleteButton.addEventListener("click", function () {
+			li.classList.add("delete");
+		});
+	}
+
+	function addListAfterClick() {
+		if (getInputLength() > 0) {
+			createListElement();
+		}
+	}
+
+	function addListAfterKeypress(event) {
+		if (getInputLength() > 0 && event.keyCode === 13) {
+			createListElement();
+		}
+	}
+
+	enterButton.addEventListener("click", addListAfterClick);
+	input.addEventListener("keypress", addListAfterKeypress);
+});
